5cf48d4753701396a69c17856eb50d88a963fc00
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-10-15 12:57:17 -0500

    adding files and repo
    lxr-contraband

---
+++ server/server.lua
@@ -0,0 +1,11 @@
+RegisterNetEvent('lxr-sellcontraband:server:sell', function(data)
+    local src = source
+    local Player = exports['lxr-core']:GetPlayer(src)
+    if not Player then return end
+    if Player.Functions.RemoveItem(data.name, data.amount, data.slot) then
+        Player.Functions.AddMoney('cash', data.amount * data.price, "sold-contraband")
+        TriggerClientEvent('inventory:client:ItemBox', src, data.name, "remove")
+        --if math.random(1, 100) > 25 then return end
+        --TriggerEvent('lxr-witness:server:WitnessAlert', GetEntityCoords(GetPlayerPed(src)), 'Someone\'s Selling Contraband')
+    end
+end)
